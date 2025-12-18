5cc1f7fd877f5488e563748d678d6316d5c3e4bb
3bT_1 <wavyware1@proton.me>
2025-12-18 07:45:21 +0100

    Add dziennik file

---
+++ src/pl/zsgornik/DziennikLekcyjny.java
@@ -0,0 +1,10 @@
+package pl.zsgornik;
+
+import java.util.List;
+
+public class DziennikLekcyjny {
+    private List<Uczen> students;
+    private List<Nauczyciel> teachers;
+    private List<Przedmiot> subjects;
+
+}
