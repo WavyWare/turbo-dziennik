0dc43d2bf23ad22a553c0463a1e730a0a037f1d9
3bT_1 <wavyware1@proton.me>
2025-12-18 07:21:43 +0100

    Add basic grade class

--- src/pl/zsgornik/Ocena.java
+++ src/pl/zsgornik/Ocena.java
@@ -1,4 +1,32 @@
 package pl.zsgornik;
 
 public class Ocena {
+    private int value;
+    private String comment;
+
+    public Ocena(int value, String comment) {
+        this.value = value;
+        this.comment = comment;
+    }
+
+    public Ocena(int value) {
+        this.value = value;
+        comment = "Brak komentarza.";
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
