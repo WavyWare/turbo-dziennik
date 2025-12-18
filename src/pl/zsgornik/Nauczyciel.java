ce6008057f3dd1e4625fc802c036f76aff2a156a
3bT_1 <wavyware1@proton.me>
2025-12-18 08:27:18 +0100

    Add login options for teachers

--- src/pl/zsgornik/Nauczyciel.java
+++ src/pl/zsgornik/Nauczyciel.java
@@ -4,10 +4,14 @@ public class Nauczyciel {
     private String fullName;
     private static int nextId = 1;
     private final int id;
+    private String username;
+    private String passwordHash;
 
-    public Nauczyciel(String fullName) {
-        this.fullName = fullName;
+    public Nauczyciel(String fullName, String username, String password) {
         this.id = nextId++;
+        this.fullName = fullName;
+        this.username = username;
+        this.passwordHash = Util.hash(password);
     }
 
     public String getFullName() {
@@ -20,6 +24,18 @@ public class Nauczyciel {
 
     public int getId() {
         return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override
